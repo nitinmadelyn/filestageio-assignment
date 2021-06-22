e8505aeb2966421077f4e3fa71f1b82f4e4963ff
nitinmadelyn <nitin.madelyn@gmail.com>
2021-06-22 20:35:48 +0530

    add dbConnectString to config

--- backend/src/config/config.js
+++ backend/src/config/config.js
@@ -1,5 +1,7 @@
 module.exports = {
   port: Number(process.env.port) || 3001,
+  dbConnectString:
+    process.env.dbConnectString || "mongodb://localhost:27017/todos",
   itemsPerPage: process.env.itemsPerPage || 20,
   apiEndPoint: process.env.apiEndPoint || "http://localhost:3001/",
 };
--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose");
-
+const config = require("./config");
 const database = module.exports;
 
 database.connect = async function connect() {
-  database.client = await mongoose.connect("mongodb://localhost:27017/todos", {
+  database.client = await mongoose.connect(config.dbConnectString, {
     useUnifiedTopology: true,
   });
 };
